f2becea4aa87871546e26b9c97cf04bdbe6725dc
Larissa Albuquerque <larissaanogueira24@gmail.com>
2025-01-13 09:57:57 +0100

    Add affiche()

--- Othello/src/main/java/com/mycompany/othello/TourDeJeu.java
+++ Othello/src/main/java/com/mycompany/othello/TourDeJeu.java
@@ -11,13 +11,28 @@ import java.util.Scanner;
  * @author Clara
  */
 public class TourDeJeu {
+    
+    /**
+     * Verifica se a entrada está no formato correto (letra de 'a' a 'h' e número de '1' a '8').
+     * 
+     * @param input A entrada do jogador.
+     * @return true se a entrada for válida, false caso contrário.
+     */
+    public static boolean isValidEntry(String input) {
+        return input != null && input.matches("^[a-h][1-8]$");
+    }
+    
+    /**
+     * Main class
+     * @param args 
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input;
         boolean isValid;
-        Tableau tableau = new Tableau();
         
         //Creation du tableau du jeu et des joueurs
+        Tableau tableau = new Tableau();
         Joueur joueur1 = new Joueur();
         Joueur joueur2 = new Joueur();
 
@@ -25,8 +40,8 @@ public class TourDeJeu {
         joueur2.setNom("Clara");
 
         //Debut du jeu
-        
         tableau.init();
+        tableau.affiche();
 
         do {
             System.out.print("Entrée de 'a1' a 'h8': ");
@@ -40,15 +55,5 @@ public class TourDeJeu {
 
         System.out.println("Entrada válida: " + input);
         scanner.close();
-    }
-
-    /**
-     * Verifica se a entrada está no formato correto (letra de 'a' a 'h' e número de '1' a '8').
-     * 
-     * @param input A entrada do jogador.
-     * @return true se a entrada for válida, false caso contrário.
-     */
-    public static boolean isValidEntry(String input) {
-        return input != null && input.matches("^[a-h][1-8]$");
     }
 }
